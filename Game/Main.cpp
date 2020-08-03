921c72ca2dbea60aed0a002d03687d23a958c167
Christian Vaughn <CVaughn@student.neumont.edu>
2020-08-03 10:56:36 -0600

    Initial Project Creation

--- Game/Main.cpp
+++ Game/Main.cpp
@@ -1,11 +1,32 @@
-// Game.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Main.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
+#include "SDL.h"
 
-int main()
+int main(int, char**)
 {
-    std::cout << "Hello World!\n";
+	if (SDL_Init(SDL_INIT_VIDEO) != 0)
+	{
+		std::cout << "SDL_INIT Error: " << SDL_GetError() << std::endl;
+		return 1;
+	}
+
+	SDL_Window* window = SDL_CreateWindow("GAT150", 100, 100, 800, 600, SDL_WINDOW_SHOWN);
+
+	if (window == nullptr) {
+		std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+	}
+
+	// wait for keyboard enter to exit
+	std::getchar();
+
+
+	SDL_Quit();
+
+	return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
